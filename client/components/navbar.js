84185be31c6b0e1bc744aa715cc702ba2ed09225
Kendall Moraski <kendimoraski@gmail.com>
2021-03-03 13:05:50 -0500

    style: choose Candal and Yellowtail fonts

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOUTIQUE CANDLES</h1>
+    <h1 id="site-title">BOUTIQUE CANDLES</h1>
     <nav>
       {isLoggedIn ? (
         <div>
