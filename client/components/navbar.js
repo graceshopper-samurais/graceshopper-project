2eb164c76c95403c5f66d3f7f1781fd6a6190115
Kendall Moraski <kendimoraski@gmail.com>
2021-03-03 14:24:22 -0500

    style: center title

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,9 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 id="site-title">BOUTIQUE CANDLES</h1>
+    <div id="title-container">
+      <h1 id="site-title">BOUTIQUE CANDLES</h1>
+    </div>
     <nav>
       {isLoggedIn ? (
         <div>
