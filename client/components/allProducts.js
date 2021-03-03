9651bdfd70bfc699647862ec5f1a2b803bfcc204
Kendall Moraski <kendimoraski@gmail.com>
2021-03-03 17:12:30 -0500

    style: align text under image, style: establish responsiveness for media queries 3 sizes

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -11,33 +11,29 @@ export class AllProducts extends React.Component {
   render() {
     const products = this.props.products
     return (
-      <div>
-        <div id="all-products">
-          {products.length ? (
-            products.map(product => {
-              return (
-                <div id="single-product" key={product.id}>
-                  <div id="product-image-container">
-                    <img src={product.imageUrl} />
+      <div id="all-products-container">
+        {products.length ? (
+          products.map(product => {
+            return (
+              <div id="single-product-container" key={product.id}>
+                <img src={product.imageUrl} />
+                <div id="product-name-price-container">
+                  <div id="product-name-container">
+                    <Link to={`/products/${product.id}`}>
+                      <span>{product.name}</span>
+                    </Link>
                   </div>
-                  <div id="product-name-price-container">
-                    <div id="product-name-container">
-                      <Link to={`/products/${product.id}`}>
-                        <span>{product.name}</span>
-                      </Link>
-                    </div>
-                    <div id="product-price-container">
-                      <span>{product.price}</span>
-                    </div>
-                    <button id="add-to-cart"> Add To Cart </button>
+                  <div id="product-price-container">
+                    <span>{product.price}</span>
                   </div>
+                  <button id="add-to-cart"> Add To Cart </button>
                 </div>
-              )
-            })
-          ) : (
-            <p>Candles Loading...</p>
-          )}
-        </div>
+              </div>
+            )
+          })
+        ) : (
+          <p>Candles Loading...</p>
+        )}
       </div>
     )
   }
