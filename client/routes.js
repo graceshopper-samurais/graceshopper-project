6924a6cf25f82f2b899f5a35453f72fa8062207f
Kendall Moraski <kendimoraski@gmail.com>
2021-03-02 21:27:55 -0500

    feat: connect AllUsers Route to client/routes.js

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, allProducts} from './components'
+import {Login, Signup, UserHome, allProducts, AllUsers} from './components'
 import {me} from './store'
 
 /**
