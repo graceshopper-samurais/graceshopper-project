e71eb4b143b8cb0814a4f330a7da7b6baf2ae522
katelyn devine <katelyndevine.design@gmail.com>
2021-03-03 14:04:04 -0600

    smaller icon

---
+++ client/components/CartIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {cart} from '../../public/icons'
+
+const CartIcon = () => {
+  return (
+    <div>
+      {/* <img src={cart} /> */}
+      <img src="./icons/cart.png" className="cartIcon" /> 0
+    </div>
+  )
+}
+
+export default CartIcon
---
+++ client/components/FullCart.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FullCart = () => {
+  return <div>To be created :D</div>
+}
+
+export default FullCart
