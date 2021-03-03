b97197167958cc460fa7acfd2dec713000e08516
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-02 20:26:42 -0600

    single product finished sans styling

--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,14 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import allUsersReducer from './allUsers'
 import products from './products'
+import singleProduct from './singleProduct'
 
-const reducer = combineReducers({user, products, allUsers: allUsersReducer})
+const reducer = combineReducers({
+  user,
+  products,
+  allUsers: allUsersReducer,
+  singleProduct
+})
 
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
