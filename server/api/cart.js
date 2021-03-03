a58c9bc7241f79418c5c063b44a22f00d0f251ba
katelyn devine <katelyndevine.design@gmail.com>
2021-03-03 15:41:29 -0600

    cart backend GET route

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,15 +1,18 @@
 const router = require('express').Router()
-const {User, Cart, Product} = require('../db/models')
+const {Cart} = require('../db/models')
 module.exports = router
 
 // GET /api/users/:id/cart
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const userCart = await Cart.findAll({
-//       where:
-//     })
-//     res.json(singleUser)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+router.get('/:id/cart', async (req, res, next) => {
+  try {
+    const userCart = await Cart.findAll({
+      where: {
+        userId: req.params.id
+      },
+      include: [Product]
+    })
+    res.json(userCart)
+  } catch (err) {
+    next(err)
+  }
+})
