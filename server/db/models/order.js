68ac918f58a57ba57ed45a73078d58fd4ae6f3cc
Kendall Moraski <kendimoraski@gmail.com>
2021-03-04 16:39:02 -0500

    chore: build Order model

---
+++ server/db/models/order.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  isFulfilled: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+})
+
+module.exports = Order
