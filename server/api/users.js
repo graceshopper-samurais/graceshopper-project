14132a30ffdd900442ad9c60ca437af7867bb2d9
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-03 21:02:06 -0500

    feat(api): add security to GET api/users

--- server/api/users.js
+++ server/api/users.js
@@ -4,16 +4,22 @@ module.exports = router
 
 // GET /api/users
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
+  if (typeof req.user === 'undefined' || !req.user.admin) {
+    const err = new Error('Access Denied')
+    err.status = 403
     next(err)
+  } else {
+    try {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email']
+      })
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
   }
 })
 
