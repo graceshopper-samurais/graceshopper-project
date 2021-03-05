e1e044ad7e1ee9c79478b94ba4165bc0bbf1c868
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-04 21:51:25 -0600

    feat: delete button

--- server/api/users.js
+++ server/api/users.js
@@ -54,14 +54,24 @@ router.get('/:id', async (req, res, next) => {
 // GET /api/users/:id/cart
 router.get('/:id/cart', async (req, res, next) => {
   try {
-    const userCart = await Order.findAll({
+    const userCart = await Order.findOne({
       where: {
         userId: req.params.id,
-        isFullfilled: false // will this be boolean?
+        isFulfilled: false // will this be boolean?
       },
       include: [ProductOrder]
     })
-    res.json(userCart)
+    res.json(userCart.productorders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//This would be logical hierarchy for the route, but not using the UserId at this point
+router.delete('/:id/cart/:line', async (req, res, next) => {
+  try {
+    await ProductOrder.destroy({where: {id: req.params.line}})
+    res.status(204).send()
   } catch (err) {
     next(err)
   }
