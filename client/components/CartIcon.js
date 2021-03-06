1455da4e61a72fee8cdfcdda1fc8d7630834183c
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-06 17:37:09 -0600

    fix: cart icon now tracks number of line items

--- client/components/CartIcon.js
+++ client/components/CartIcon.js
@@ -2,25 +2,24 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
-const CartIcon = props => {
+const CartIcon = (props) => {
   console.log('this is props from CARTICON', props)
   return (
     <div>
       <Link to={`/users/${props.user.id}/cart`}>
         <img src="/icons/cart.png" className="cartIcon" />{' '}
-        {/* {props.cart.reduce((totalItems, cartLine) => {
+        {props.cart.reduce((totalItems, cartLine) => {
           return totalItems + cartLine.quantity
-        }, 0) } */}
-        0
+        }, 0)}
       </Link>
     </div>
   )
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     user: state.user,
-    cart: state.singleCart.cart
+    cart: state.singleCart.cart,
   }
 }
 
