b16387ecd9dd8693b50bca2aa06f54e8afa13e55
katelyn devine <katelyndevine.design@gmail.com>
2021-03-06 09:32:41 -0600

    guestCart start

--- client/components/FullCart.js
+++ client/components/FullCart.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchCart} from '../store/singleCart'
 import DeleteButton from './DeleteButton'
+import GuestCart from './GuestCart'
 
 class FullCart extends React.Component {
   componentDidMount() {
@@ -16,46 +17,50 @@ class FullCart extends React.Component {
   render() {
     const {cart} = this.props
     // console.log('props from FULLCART: ', this.props)
-    if (this.props.noCart) {
-      return (
-        <div className="cart__cart-header">
-          <p>No items currently in your cart. Happy shopping!</p>
-        </div>
-      )
-    } else {
-      return (
-        <div className="cart__cart-header">
-          <div> You have {cart.length} items in your cart </div>
+    if (this.props.isLoggedIn) {
+      if (this.props.noCart) {
+        return (
+          <div className="cart__cart-header">
+            <p>No items currently in your cart. Happy shopping!</p>
+          </div>
+        )
+      } else {
+        return (
+          <div className="cart__cart-header">
+            <div> You have {cart.length} items in your cart </div>
 
-          {cart.map(item => {
-            return (
-              <div key={item.id}>
-                <img
-                  src={item.product.imageUrl}
-                  className="cartImg"
-                  alt={item.product.name}
-                />
-                <div> {item.product.name} </div>
-                <div> Quantity: {item.quantity} </div>
-                <select>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                </select>
-                <DeleteButton
-                  productOrderId={item.id}
-                  userId={this.props.match.params.id}
-                />
-              </div>
-            )
-          })}
-        </div>
-      )
+            {cart.map(item => {
+              return (
+                <div key={item.id}>
+                  <img
+                    src={item.product.imageUrl}
+                    className="cartImg"
+                    alt={item.product.name}
+                  />
+                  <div> {item.product.name} </div>
+                  <div> Quantity: {item.quantity} </div>
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                  </select>
+                  <DeleteButton
+                    productOrderId={item.id}
+                    userId={this.props.match.params.id}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        )
+      }
+    } else {
+      return <GuestCart />
     }
   }
 }
@@ -63,7 +68,8 @@ class FullCart extends React.Component {
 const mapState = state => {
   return {
     cart: state.singleCart.cart,
-    noCart: state.singleCart.noCart
+    noCart: state.singleCart.noCart,
+    isLoggedIn: !!state.user.id
   }
 }
 
