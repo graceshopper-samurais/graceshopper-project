d892890d793fdc4bb5c6b6bd35ccf520a8d858b3
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-08 18:47:13 -0500

    bug(react): Set quantity maximum to 8

--- client/components/AddToCartButton.js
+++ client/components/AddToCartButton.js
@@ -1,32 +1,70 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {addToCartThunk} from '../store/singleCart'
+import {addToGuestCartThunk} from '../store/guestCart'
 
-const AddToCartButton = ({product, userId, addToCart}) => {
-  return (
-    <div>
-      <button
-        type="button"
-        id="add-to-cart"
-        onClick={() => addToCart(userId, product.id)}
-      >
-        {' '}
-        Add To Cart{' '}
-      </button>
-    </div>
-  )
+// const {product, userId, addToCart} = this.props
+
+class AddToCartButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    if (this.props.isLoggedIn) {
+      const cart = this.props.userCart
+      console.log('cart——————', cart)
+      const indx = cart
+        .map(item => item.product.id)
+        .indexOf(this.props.product.id)
+      if (indx > -1 && cart[indx].quantity >= 8) {
+        alert('There is a maximum of eight items!')
+      } else {
+        this.props.addToCart(this.props.userId, this.props.product.id)
+      }
+    } else {
+      const cart = this.props.guestCart
+      console.log('this.props———————', this.props)
+      const indx = cart.map(item => item.id).indexOf(this.props.product.id)
+      if (indx > -1 && cart[indx].quantity >= 8) {
+        alert('There is a maximum of eight items!')
+      } else {
+        this.props.addToGuestCart(this.props.product.id)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          id="add-to-cart"
+          onClick={() => this.handleClick()}
+        >
+          {' '}
+          Add To Cart{' '}
+        </button>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    userId: state.user.id
+    userId: state.user.id,
+    isLoggedIn: !!state.user.id,
+    userCart: state.singleCart.cart,
+    guestCart: state.guestCart.guestCart
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     addToCart: (userId, productId) =>
-      dispatch(addToCartThunk(userId, productId))
+      dispatch(addToCartThunk(userId, productId)),
+    addToGuestCart: productId => dispatch(addToGuestCartThunk(productId))
   }
 }
 
--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -13,10 +13,33 @@ export class AllProducts extends React.Component {
     this.state = {
       showAddProduct: false
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     this.props.getProducts()
+  }
+
+  handleClick(productId) {
+    if (this.props.isLoggedIn) {
+      const cart = this.props.userCart
+      console.log('cart——————', cart)
+      const indx = cart.map(item => item.product.id).indexOf(productId)
+      if (indx > -1 && cart[indx].quantity >= 8) {
+        alert('There is a maximum of eight items!')
+      } else {
+        this.props.addToCart(this.props.userId, productId)
+      }
+    } else {
+      const cart = this.props.guestCart
+      console.log('this.props———————', this.props)
+      const indx = cart.map(item => item.id).indexOf(productId)
+      if (indx > -1 && cart[indx].quantity >= 8) {
+        alert('There is a maximum of eight items!')
+      } else {
+        this.props.addToGuestCart(productId)
+      }
+    }
   }
 
   render() {
@@ -72,15 +95,7 @@ export class AllProducts extends React.Component {
                       type="button"
                       id="add-to-cart"
                       // if user is logged in add item to user cart, else add item to guest cart/local storage
-                      onClick={
-                        this.props.isLoggedIn
-                          ? () =>
-                              this.props.addToCart(
-                                this.props.userId,
-                                product.id
-                              )
-                          : () => this.props.addToGuestCart(product.id)
-                      }
+                      onClick={() => this.handleClick(product.id)}
                     >
                       {' '}
                       Add To Cart{' '}
@@ -121,7 +136,9 @@ const mapState = state => {
     loading: state.products.loading,
     userId: state.user.id,
     isLoggedIn: !!state.user.id,
-    isAdmin: state.user.admin
+    isAdmin: state.user.admin,
+    userCart: state.singleCart.cart,
+    guestCart: state.guestCart.guestCart
   }
 }
 
