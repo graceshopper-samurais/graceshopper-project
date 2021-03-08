be9e595ea950a1993500d0dc88bf3d09f8a57183
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-08 15:42:05 -0500

    fix(react): mediocre fix for cartIcon while loading data

--- client/components/CartIcon.js
+++ client/components/CartIcon.js
@@ -7,35 +7,54 @@ import {fetchCart} from '../store/singleCart'
 class CartIcon extends React.Component {
   componentDidMount() {
     if (this.props.isLoggedIn) {
+      console.log('component did mount, fetch USER cart ——————')
       this.props.fetchUserCart(this.props.user.id)
     } else {
+      console.log('component did mount, fetch GUEST cart ——————')
       this.props.fetchGuestCart()
     }
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.isLoggedIn !== this.props.isLoggedIn) {
+      if (this.props.isLoggedIn) {
+        console.log('reaching fetch USER cart—————')
+        this.props.fetchUserCart(this.props.user.id)
+      } else {
+        console.log('reaching fetch GUEST cart—————')
+        this.props.fetchGuestCart()
+      }
+    }
+  }
+
   render() {
-    console.log(typeof this.props.guestCart)
-    console.log('this.props.guestCart——————', this.props.guestCart)
-    // const guestCart = this.props.guestCart ? this.props.guestCart : []
+    console.log('in render——————————')
+    console.log('PROPS IN RENDER———————', this.props)
     if (this.props.isLoggedIn) {
+      console.log('IS LOGGED IN————————')
       return (
         <div>
           <Link to={`/users/${this.props.user.id}/cart`}>
             <img src="/icons/cart.png" className="cartIcon" />{' '}
-            {this.props.cart.reduce((totalItems, cartLine) => {
-              return totalItems + cartLine.quantity
-            }, 0)}
+            {this.props.loading
+              ? ' '
+              : this.props.cart.reduce((totalItems, cartLine) => {
+                  return totalItems + cartLine.quantity
+                }, 0)}
           </Link>
         </div>
       )
     } else if (!this.props.isLoggedIn) {
+      console.log('NOT LOGGED IN————————')
       return (
         <div>
           <Link to={`/users/${this.props.user.id}/cart`}>
             <img src="/icons/cart.png" className="cartIcon" />{' '}
-            {this.props.guestCart.reduce((totalItems, cartLine) => {
-              return totalItems + cartLine.quantity
-            }, 0)}
+            {this.props.loading
+              ? ' '
+              : this.props.guestCart.reduce((totalItems, cartLine) => {
+                  return totalItems + cartLine.quantity
+                }, 0)}
           </Link>
         </div>
       )
@@ -48,7 +67,8 @@ const mapState = state => {
     user: state.user,
     cart: state.singleCart.cart,
     isLoggedIn: !!state.user.id,
-    guestCart: state.guestCart.guestCart
+    guestCart: state.guestCart.guestCart,
+    loading: state.products.loading
   }
 }
 
