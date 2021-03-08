bbbf4ede4e2ae12e579ec08fa425f8ee8a68aa72
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-07 19:19:31 -0500

    fix: fix styling and user fields on allUsers component

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -15,48 +15,50 @@ export class AllUsers extends React.Component {
     const users = this.props.users
     return (
       <div id="all-users-view">
-        <table id="all-users-table">
-          <tr id="column-names-row">
-            <th scope="col"> User ID </th>
-            <th scope="col"> Name </th>
-            <th scope="col"> Email Address </th>
-            {/* would an Admin have access to users' passwords? */}
-            <th scope="col"> Street Address </th>
-            <th scope="col"> City </th>
-            <th scope="col"> State </th>
-            <th scope="col"> Zip Code </th>
-            <th scope="col"> Admin Privileges </th>
-          </tr>
-          {/* map through each user and output a row for each one */}
-          {users.map(user => (
-            <React.Fragment key={user.id}>
-              <tr id="single-user-row">
-                <th scope="row"> {user.id} </th>
-                <td> {user.name} </td>
-                <td> {user.email} </td>
-                <td> {user.address} </td>
-                <td> {user.city} </td>
-                <td> {user.state} </td>
-                <td> {user.zip} </td>
-                <td> {user.admin} </td>
-              </tr>
-            </React.Fragment>
-          ))}
+        <table id="all-users-table" cellSpacing="20">
+          <tbody>
+            <tr id="column-names-row">
+              <th scope="col"> USER ID </th>
+              <th scope="col"> NAME</th>
+              <th scope="col"> EMAIL ADDRESS </th>
+              {/* would an Admin have access to users' passwords? */}
+              <th scope="col"> STREET ADDRESS </th>
+              <th scope="col"> CITY </th>
+              <th scope="col"> STATE </th>
+              <th scope="col"> ZIP CODE </th>
+              <th scope="col"> ADMIN </th>
+            </tr>
+            {/* map through each user and output a row for each one */}
+            {users.map((user) => (
+              <React.Fragment key={user.id}>
+                <tr id="single-user-row">
+                  <th scope="row"> {user.id} </th>
+                  <td> {user.name} </td>
+                  <td> {user.email} </td>
+                  <td> {user.address} </td>
+                  <td> {user.city} </td>
+                  <td> {user.state} </td>
+                  <td> {user.zip} </td>
+                  <td> {String(user.admin)} </td>
+                </tr>
+              </React.Fragment>
+            ))}
+          </tbody>
         </table>
       </div>
     )
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
-    users: state.allUsers.users
+    users: state.allUsers.users,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
-    fetchAllUsers: () => dispatch(fetchAllUsers())
+    fetchAllUsers: () => dispatch(fetchAllUsers()),
   }
 }
 
